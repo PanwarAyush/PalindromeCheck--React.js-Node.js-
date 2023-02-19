1fd6e22195966bf51a970490f7517a823dd12718
Ayush Panwar <ayushpanwar691@gmail.com>
2023-02-19 07:52:45 +0530

    ads

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,15 @@
 
 import NodeReact from './components/NodeReact';
 import Palindrome from './components/Palindrome';
+import AdSense from 'react-adsense';
 const App = () => {
   return (
     <div>
       <Palindrome/>
+      <AdSense.Google
+       client='ca-pub-9471515798383572'
+       slot='9263299235'
+        />
       <NodeReact/>
     </div>
   );
